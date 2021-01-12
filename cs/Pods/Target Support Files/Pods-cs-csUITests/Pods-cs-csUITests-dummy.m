e07e38eb43ac44d442399156b36f135483c8b846
Lin Li <AZLinli@163.com>
2021-01-12 16:05:19 +0800

    删除pod

--- cs/Pods/Target Support Files/Pods-cs-csUITests/Pods-cs-csUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cs_csUITests : NSObject
-@end
-@implementation PodsDummy_Pods_cs_csUITests
-@end
--- cs/Pods/Target Support Files/Pods-csTests/Pods-csTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_csTests : NSObject
-@end
-@implementation PodsDummy_Pods_csTests
-@end
